a416b59a1bd0e9e6a38985ea5d492e28cbab2f72
Cabidge <cabbageboi69@gmail.com>
2022-03-19 18:16:47 -0400

    Tweak

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod lexing;
 
-fn main() {
-    for tok in lexing::lex("let x = 12;").unwrap() {
+fn main() -> Result<(),()> {
+    for tok in lexing::lex("hello <- & 12;")? {
         println!("{}", tok.kind);
     }
+
+    Ok(())
 }
