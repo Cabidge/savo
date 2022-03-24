c442ff50c7b84ac0189ee8e683d95b5c19335423
Cabidge <cabbageboi69@gmail.com>
2022-03-24 12:35:15 -0400

    Implement export

--- src/compiling/compiler.rs
+++ src/compiling/compiler.rs
@@ -4,6 +4,15 @@ use inkwell::{
     builder::Builder,
     values::{BasicMetadataValueEnum, FloatValue, PointerValue, FunctionValue},
     AddressSpace,
+    targets::{
+        CodeModel,
+        FileType,
+        InitializationConfig,
+        RelocMode,
+        Target,
+        TargetMachine,
+    },
+    OptimizationLevel,
 };
 
 use std::collections::HashMap;
@@ -48,7 +57,32 @@ impl<'ctx> Compiler<'ctx> {
     }
 
     pub fn export(&self, out: &str) {
-        todo!();
+        Target::initialize_all(&InitializationConfig::default());
+
+        // Target based on host machine
+        let target_triple = TargetMachine::get_default_triple();
+        let cpu = TargetMachine::get_host_cpu_name().to_string();
+        let features = TargetMachine::get_host_cpu_features().to_string();
+
+        let target = Target::from_triple(&target_triple)
+            .map_err(|e| format!("{:?}", e)).unwrap();
+        
+        // Create target machine
+        let target_machine = target
+            .create_target_machine(
+                &target_triple,
+                &cpu,
+                &features,
+                OptimizationLevel::Default,
+                RelocMode::Default,
+                CodeModel::Default,
+            )
+            .ok_or_else(|| "Unable to create target machine.".to_string()).unwrap();
+
+        // Compile
+        target_machine
+            .write_to_file(&self.module, FileType::Object, out.as_ref())
+            .map_err(|e| format!("{:?}", e)).unwrap();
     }
 
     fn compile_func(&self, name: &str, block: &Block) {
