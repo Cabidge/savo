73c917d1a979987cb4b17c39703e5f269a0d1200
Cabidge <cabbageboi69@gmail.com>
2022-03-24 11:57:12 -0400

    Compiler correctly allocates local variables

--- src/compiling/compiler.rs
+++ src/compiling/compiler.rs
@@ -1,9 +1,11 @@
 use inkwell::{
     context::Context,
     module::Module,
-    values::FloatValue,
+    values::{FloatValue, PointerValue},
     AddressSpace,
 };
+
+use std::collections::HashMap;
 
 use crate::resolving::{ Program, Block, Stmt, Expr };
 
@@ -11,6 +13,8 @@ pub struct Compiler<'ctx> {
     pub ctx: &'ctx Context,
     pub module: Module<'ctx>,
 }
+
+type LocalPtrs<'ctx> = HashMap<String, PointerValue<'ctx>>;
 
 impl<'ctx> Compiler<'ctx> {
     pub fn new(ctx: &'ctx Context) -> Self {
@@ -38,7 +42,7 @@ impl<'ctx> Compiler<'ctx> {
 
         // Define functions
         for (name, block) in program.funcs.iter() {
-            //self.compile_func(block);
+            self.compile_func(name, block);
         }
     }
 
@@ -46,18 +50,44 @@ impl<'ctx> Compiler<'ctx> {
         todo!();
     }
 
-    fn compile_func(&self, block: &Block) {
-        todo!();
+    fn compile_func(&self, name: &str, block: &Block) {
+        let f64_type = self.ctx.f64_type();
+
+        // Add entry
+        let func = self.module.get_function(name).unwrap();
+        let fn_block = self.ctx.append_basic_block(func, "entry");
+
+        // Move builder
+        let builder = self.ctx.create_builder();
+        builder.position_at_end(fn_block);
+
+        // Allocate locals
+        let mut locals: LocalPtrs<'ctx> = HashMap::new();
+        for local in block.get_vars().iter() {
+            let local_ptr = builder.build_alloca(f64_type, local);
+            locals.insert(local.to_string(), local_ptr);
+        }
+
+        // Compile statements
+
+
+        // Default return value
+        let f64_zero = f64_type.const_zero();
+        builder.build_return(Some(&f64_zero));
     }
 
-    fn compile_stmt(&self, stmt: &Stmt) {
+    fn compile_stmt(&self, stmt: &Stmt, locals: &LocalPtrs<'ctx>) {
         match stmt {
-            Stmt::Expr(expr) => { self.compile_expr(expr); }
+            Stmt::Expr(expr) => { self.compile_expr(expr, locals); }
             _ => todo!(),
         }
     }
 
-    fn compile_expr(&self, expr: &Expr) ->  FloatValue<'ctx> {
-        todo!()
+    fn compile_expr(&self, expr: &Expr, locals: &LocalPtrs<'ctx>) ->  FloatValue<'ctx> {
+        let f64_type = self.ctx.f64_type();
+        match expr {
+            Expr::Val(v) => f64_type.const_float(*v),
+            _ => todo!(),
+        }
     }
 }
