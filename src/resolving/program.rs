6c5b3a7c313616ad95318cbc88de1e1d4ad06887
Cabidge <cabbageboi69@gmail.com>
2022-03-25 12:27:53 -0400

    Fixed get var name for outer scope

--- src/resolving/program.rs
+++ src/resolving/program.rs
@@ -130,7 +130,9 @@ impl<'a> SubBlock<'a> {
     }
 
     pub(super) fn get_var_name(&self, name: &str, globals: &HashMap<String, f64>) -> Option<String> {
-        self.parent.get_var_name(&format!(">{}", name), globals)
+        self.parent
+            .get_var_name(&format!(">{}", name), globals)
+            .or_else(|| parent.get_var_name(name), globals)
     }
 }
 
