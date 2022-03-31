52734974274212a9eae0a98aa92788805ffa87bb
Cabidge <cabbageboi69@gmail.com>
2022-03-31 19:14:13 -0400

    Added intrinsic deque functions

--- src/compiling/compiler/intrinsics.rs
+++ src/compiling/compiler/intrinsics.rs
@@ -18,20 +18,40 @@ pub(super) fn write_intrinsics() -> tempfile::NamedTempFile {
 pub(super) fn build<'ctx>(ctx: &'ctx Context, module: &Module<'ctx>) {
     let f64_type = ctx.f64_type();
     let void_type = ctx.void_type();
+    let ptr_type = ctx.bool_type().ptr_type();
+
+    let void_f64_fn_type = void_type.fn_type(&[f64_type.into()], false);
+
+    // -- dumpf
+    module.add_function("dumpf", void_f64_fn_type, None);
 
     // -- putfc
-    let putfc_fn_type = void_type.fn_type(&[f64_type.into()], false);
-    module.add_function("putfc", putfc_fn_type, None);
+    module.add_function("putfc", void_f64_fn_type, None);
 
     // -- getfc
     let getfc_fn_type = f64_type.fn_type(&[], false);
     module.add_function("getfc", getfc_fn_type, None);
 
-    // -- dumpf
-    let dumpf_fn_type = void_type.fn_type(&[f64_type.into()], false);
-    module.add_function("dumpf", dumpf_fn_type, None);
-
     // -- pow
     let pow_fn_type = f64_type.fn_type(&[f64_type.into(), f64_type.into()], false);
     module.add_function("pow", pow_fn_type, None);
+
+    // -- Deque functions
+    {
+        let new_deque_fn_type = ptr_type.fn_type(&[], false);
+        module.add_function("newDeque", new_deque_fn_type, None);
+
+        let del_deque_fn_type = void_type.fn_type(&[ptr_type.into()], false);
+        module.add_function("delDeque", del_deque_fn_type, None);
+
+        let f64_ptr_fn_type = f64_type.fn_type(&[ptr_type.into()], false);
+        module.add_function("sizeOfQueue", f64_ptr_fn_type, None);
+        module.add_function("peekDeque", f64_ptr_fn_type, None);
+        module.add_function("peekHeadDeque", f64_ptr_fn_type, None);
+        module.add_function("popDeque", f64_ptr_fn_type, None);
+        module.add_function("popHeadDeque", f64_ptr_fn_type, None);
+
+        let push_deque_fn_type = void.fn_type(&[ptr_type.into(), f64_type.into()], false);
+        module.add_function("pushDeque", push_deque_fn_type, None);
+    }
 }
